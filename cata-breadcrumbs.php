cf0577e5f2c8c6d85970a238bfaaf641c26ff0e8
Jessica Townsend <jessica@thoughtcatalog.com>
2024-10-16 16:29:50 -0400

    initial commit

---
+++ cata-breadcrumbs.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Cata Breadcrumbs
+ *
+ * @package   Cata\Breadcrumbs
+ * @author    Thought & Expression Co. <devjobs@thought.is>
+ * @copyright 2019 Thought & Expression Co.
+ * @license   GNU GENERAL PUBLIC LICENSE
+ *
+ * @wordpress-plugin
+ * Plugin Name: Cata Breadcrumbs
+ * Description: Add breadcrumbs to pages & posts.
+ * Author:      Thought & Expression Co. <devjobs@thought.is>
+ * Author URI:  https://thought.is
+ * Version:     0.1.0
+ * License:     GPL v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ */
