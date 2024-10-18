d63d3aa0ddb22aa59a0bffe0cf096f39f3edb47f
Jessica Townsend <jessica@thoughtcatalog.com>
2024-10-18 14:35:57 -0400

    add a function with a filter to get breadcrumbs

--- cata-breadcrumbs.php
+++ cata-breadcrumbs.php
@@ -9,10 +9,19 @@
  *
  * @wordpress-plugin
  * Plugin Name: Cata Breadcrumbs
- * Description: Add breadcrumbs to pages & posts.
+ * Description: Add a function with a filter to get breadcrumbs.
  * Author:      Thought & Expression Co. <devjobs@thought.is>
  * Author URI:  https://thought.is
  * Version:     0.1.0
  * License:     GPL v3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
+
+/**
+ * Get Cata Breadcrumbs
+ * 
+ * @return array
+ */
+function get_cata_breadcrumbs(): array {
+	return apply_filters( 'get_cata_breadcrumbs', array() );
+}
